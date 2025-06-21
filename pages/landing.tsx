eb7e8af6d7ed03e0f12dcb4b37c95a87050e268f
kaoriishige <adtown001@gmail.com>
2025-06-21 22:36:29 +0900

    fix: landing page with Tailwind

---
+++ pages/landing.tsx
@@ -0,0 +1,8 @@
+export default function LandingPage() {
+  return (
+    <div style={{ textAlign: "center" as const, marginTop: "100px", fontSize: "24px", color: "#f43f5e" }}>
+      みんなの那須アプリ - 公開準備中！
+    </div>
+  )
+}
+
